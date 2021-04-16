e3dd49133ce2e28b7ee639b61285e4806c3e6d18
1papaya <1love1papaya@gmail.com>
2021-04-16 14:15:06 +0100

    add file tile cache test

---
+++ test/test-file-tile-cache.ts
@@ -0,0 +1,51 @@
+import type { TileCoord } from "../src/index";
+import { ZFactory, AWSTileSource, FileTileCache } from "../src/index";
+import * as assert from "assert";
+import * as _ from "./common";
+import * as fs from "fs";
+
+describe("File Tile Cache", function () {
+  let source = new AWSTileSource({ verbose: true });
+  let cache = new FileTileCache("./test/tmp/cache");
+  let factory = new ZFactory(source, cache);
+  let sampleCoords = _.loadJson(
+    "./test/data/correct/random-points.geojson"
+  ).features.map((feat) => feat.geometry.coordinates);
+
+  before(function () {
+    if (fs.existsSync("./test/tmp"))
+      fs.rmdirSync("./test/tmp", { recursive: true });
+
+    fs.mkdirSync("./test/tmp/cache", { recursive: true });
+  });
+
+  it(`correctly downloads files from source`, async function () {
+    for (let i = 0; i < sampleCoords.length; i++) {
+      await factory.getZ(sampleCoords[i], 12);
+    }
+
+    ["1844/1706", "1844/1707", "1845/1706", "1845/1707"].forEach(
+      (tileCoord) => {
+        assert(fs.existsSync(`./test/tmp/cache/12/${tileCoord}.png`));
+      }
+    );
+  });
+
+  it(`correctly uses saved files when exists`, async function () {
+    for (const tileCoord of [
+      "1844/1706",
+      "1844/1707",
+      "1845/1706",
+      "1845/1707",
+    ]) {
+      const fullTileCoord = [
+        12,
+        ...tileCoord.split("/").map((str) => parseInt(str)),
+      ] as TileCoord;
+
+      cache.delete(fullTileCoord);
+      const data = await cache.get(fullTileCoord);
+      assert(typeof data !== "undefined")
+    }
+  });
+});
